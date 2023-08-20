28b46b41169c7370c0921747412ef836d1fbf7ad
Rahul Arora <rahularo71171@gmail.com>
2023-08-20 20:18:29 +0530

    Added some code for production

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,18 @@
-const connectToMongo = require("./db");
-const cors = require("cors");
-const express = require("express");
+const connectToMongo = require('./db');
+const cors = require('cors');
+const express = require('express');
 const app = express();
+const path = require('path');
 const port = 5000;
 
 // Available routs
 app.use(cors());
 
 app.use(express.json());
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/notes", require("./routes/notes"));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
-app.get("/", (req, res) => {
-  var isDbConnected = connectToMongo();
-  res.send({ status: isDbConnected });
-});
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
